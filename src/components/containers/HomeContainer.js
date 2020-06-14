62339ffbd90fbdb793031402479b4a1640a066d7
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-14 18:15:39 -0400

    Added Home container and view files

---
+++ src/components/containers/HomeContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { HomeView } from '../views';
+
+export class HomeContainer extends Component {
+  render() {
+    return (
+      <div>
+        <HomeView />
+      </div>
+    );
+  }
+}
+
+export default HomeContainer;
