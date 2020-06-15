a044949a624b9da2c56f08bc1b971bbfa537727a
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-15 03:24:23 -0400

    chore: added more styles and card to view

--- src/components/views/AllStudentsView.js
+++ src/components/views/AllStudentsView.js
@@ -1,23 +1,86 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Card, Button, CardActions, CardMedia } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+}));
 
 function AllStudentsView(props) {
+  const classes = useStyles();
+
   return (
     <div>
       <div className='addStudent'>
-        <Link to='/students/new' className='add-campus'>
+        <Button
+          onClick={props.handleNew}
+          variant='contained'
+          color='primary'
+          startIcon={<AddIcon></AddIcon>}
+          style={{ padding: '15px', margin: '15px' }}
+        >
           Add Student
-        </Link>
+        </Button>
       </div>
       {props.allStudents.length > 0 ? (
         props.allStudents.map((students) => {
           return (
             <div key={students.id}>
-              <Link to={`/students/${students.id}`}>
-                <h1>
-                  {students.firstName} {students.lastName}
-                </h1>
-              </Link>
+              <Card
+                className={classes.root}
+                variant='outlined'
+                style={CardStyle}
+              >
+                <CardMedia
+                  className={classes.media}
+                  image={students.imageURL}
+                ></CardMedia>
+                <Link to={`/students/${students.id}`}>
+                  <h1>
+                    {students.firstName} {students.lastName}
+                  </h1>
+                </Link>
+                <CardActions>
+                  <Button
+                    onClick={() => props.handleDelete(students.id)}
+                    color='secondary'
+                    variant='contained'
+                    style={{ padding: '5px' }}
+                  >
+                    Delete
+                  </Button>
+
+                  <Button
+                    onClick={() => props.handleEdit(students.id)}
+                    color='primary'
+                    variant='contained'
+                    style={{ padding: '5px' }}
+                    startIcon={<EditIcon></EditIcon>}
+                  >
+                    Edit
+                  </Button>
+                </CardActions>
+              </Card>
+              <br></br>
             </div>
           );
         })
@@ -27,5 +90,8 @@ function AllStudentsView(props) {
     </div>
   );
 }
+const CardStyle = {
+  backgroundColor: '#303030',
+};
 
 export default AllStudentsView;
