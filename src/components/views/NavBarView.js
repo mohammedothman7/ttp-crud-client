c03082d2297909a3f672adbdf38b8083d0a7efc2
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-15 03:25:12 -0400

    feat: added styles to component

--- src/components/views/NavBarView.js
+++ src/components/views/NavBarView.js
@@ -1,21 +1,30 @@
-import React from "react";
-import "./styles/NavBarView.css";
-import { Link } from "react-router-dom";
+import React from 'react';
+import './styles/NavBarView.css';
+import { Link } from 'react-router-dom';
 
 const NavBarView = (props) => {
   return (
-    <nav>
-      <Link to="/" className="nav-link">
+    <nav style={style}>
+      <Link to='/' className='nav-link' style={linkStyle}>
         Home
       </Link>
-      <Link to="/campuses" className="nav-link">
+      <Link to='/campuses' className='nav-link' style={linkStyle}>
         Campuses
       </Link>
-      <Link to="/students" className="nav-link">
+      <Link to='/students' className='nav-link' style={linkStyle}>
         Students
       </Link>
     </nav>
   );
+};
+
+const style = {
+  backgroundColor: '#474745',
+};
+const linkStyle = {
+  color: '#fff',
+  fontSize: '32px',
+  textDecoration: 'none',
 };
 
 export default NavBarView;
