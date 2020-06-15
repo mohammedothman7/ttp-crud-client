14f4fe27422dd78a0844d41fc804509e4436a79e
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-14 20:36:09 -0400

    added AddStudentFormView to views barrel file

--- src/components/views/index.js
+++ src/components/views/index.js
@@ -8,3 +8,4 @@ export { default as StudentNameListView } from './StudentNameListView';
 export { default as AllStudentsView } from './AllStudentsView';
 export { default as HomeView } from './HomeView';
 export { default as StudentView } from './StudentView';
+export { default as AddStudentFormView } from './AddStudentFormView';
