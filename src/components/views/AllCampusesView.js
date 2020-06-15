5e6380e19c3becc9fe6bbfe583848f31965557ff
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-14 20:35:02 -0400

    chore: cleaned up file and format

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -1,16 +1,16 @@
-import React from "react";
-import "./styles/AllCampusesView.css";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import React from 'react';
+import './styles/AllCampusesView.css';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const AllCampusesView = (props) => {
   if (!props.allCampuses.length) {
-    return <div className="all-campuses">There are no campuses</div>;
+    return <div className='all-campuses'>There are no campuses</div>;
   }
 
   return (
-    <div className="all-campuses">
-      <Link to="/campuses/new" className="add-campus">
+    <div className='all-campuses'>
+      <Link to='/campuses/new' className='add-campus'>
         New Campus
       </Link>
       <div>
@@ -19,7 +19,7 @@ const AllCampusesView = (props) => {
             <Link to={`/campuses/${campus.id}`}>
               <h1>{campus.name}</h1>
             </Link>
-            <img src={campus.imageUrl} width="200px" alt={campus.name} />
+            <img src={campus.imageUrl} width='200px' alt={campus.name} />
             <p>{campus.students.length} students</p>
             <button onClick={() => props.handleDelete(campus.id)}>
               Delete
